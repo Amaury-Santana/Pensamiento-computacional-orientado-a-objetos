3321552c358a2bbd0d3ee1e82cb4103a2b103c9e
Amaury-Santana <amaurysantana070@gmail.com>
2024-11-10 22:56:09 -0600

    cliente.cpp

--- cliente.cpp
+++ cliente.cpp
@@ -1,1 +1,15 @@
+#include "Cliente.h"
+#include <iostream>
+using namespace std;
+
+//Definir que el constructor pertenece a la clase Cliente
+Cliente::Cliente(string n, string t, int e, string fp) : nombre(n), telefono(t), edad(e), formaDePago(fp) {}
+
+void Cliente::mostrarInfoCliente() {
+    cout << "Nombre: " << nombre << ", Teléfono: " << telefono << ", Edad: " << edad
+         << ", Forma de Pago: " << formaDePago << endl;
+}
+
+string Cliente::getNombre() { return nombre; }
+int Cliente::getEdad() { return edad; }
 
