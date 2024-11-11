8538f2cc6c4096427bc37aab51f307f1a44e1e3c
Amaury-Santana <amaurysantana070@gmail.com>
2024-11-10 22:57:06 -0600

    cliente.h

--- cliente.h
+++ cliente.h
@@ -1,1 +1,22 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+#include <string>
+using namespace std;
+//Definir clases privadasy luego publicas para el cpp
+class Cliente {
+private:
+    string nombre;
+    string telefono;
+    int edad;
+    string formaDePago;
+
+public:
+    Cliente(string n, string t, int e, string fp);
+    void mostrarInfoCliente();
+    string getNombre();
+    int getEdad();
+};
+
+#endif
 
