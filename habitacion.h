7f7b0029f925000096f49ecc19688d056c06a463
Amaury-Santana <amaurysantana070@gmail.com>
2024-11-10 23:10:35 -0600

    habitacion.h

--- habitacion.h
+++ habitacion.h
@@ -1,1 +1,21 @@
+#ifndef HABITACION_H
+#define HABITACION_H
 
+#include <string>
+using namespace std;
+
+//Crear la clase privada y la pública parta ejecutarla en el compilador
+class Habitacion {
+private:
+    int numeroHabitacion;
+    string tipo;
+    bool disponible;
+    float precio;
+
+public:
+    Habitacion(int num, string t, bool disp, float p);
+    void mostrarInfoHabitacion();
+    void cambiarDisponibilidad(bool estado);
+};
+
+#endif
