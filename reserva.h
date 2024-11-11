bea986a218f9353528fa2b24f8ea7472c392b66c
Amaury-Santana <amaurysantana070@gmail.com>
2024-11-10 23:04:55 -0600

    reserva.h

--- reserva.h
+++ reserva.h
@@ -1,1 +1,21 @@
+#ifndef RESERVA_H
+#define RESERVA_H
+
+#include <string>
+#include "cliente.h"
+using namespace std;
+
+class Reserva {
+private:
+    int idReserva;
+    Cliente cliente;
+    string fechaInicio;
+    string fechaFin;
+
+public:
+    Reserva(int id, Cliente c, string inicio, string fin);
+    void mostrarDetallesReserva();
+};
+
+#endif
 
