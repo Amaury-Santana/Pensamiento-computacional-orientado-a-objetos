d3059e1299358a873ed55a0046b1c186365d8a65
Amaury-Santana <amaurysantana070@gmail.com>
2024-11-10 23:06:13 -0600

    habitacion.cpp

--- habitacion.cpp
+++ habitacion.cpp
@@ -1,1 +1,16 @@
+#include "habitacion.h"
+#include <iostream>
+using namespace std;
+
+Habitacion::Habitacion(int num, string t, bool disp, float p)
+    : numeroHabitacion(num), tipo(t), disponible(disp), precio(p) {}
+
+void Habitacion::mostrarInfoHabitacion() {
+    cout << "Habitación No: " << numeroHabitacion << ", Tipo: " << tipo << ", Precio: $" << precio 
+         << ", Disponible: " << (disponible ? "Sí" : "No") << endl;
+}
+
+void Habitacion::cambiarDisponibilidad(bool estado) {
+    disponible = estado;
+}
 
