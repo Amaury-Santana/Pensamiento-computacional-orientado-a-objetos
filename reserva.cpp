bff8a52cb6566b85d2686c5616ef3b9ba60107b2
Amaury-Santana <amaurysantana070@gmail.com>
2024-11-10 22:58:04 -0600

    reserva.cpp

--- reserva.cpp
+++ reserva.cpp
@@ -1,1 +1,12 @@
+#include "Reserva.h"
+#include <iostream>
+using namespace std;
 
+//Definir que el constructor pertenece a la clase Reserva
+Reserva::Reserva(int id, Cliente c, string inicio, string fin)
+    : idReserva(id), cliente(c), fechaInicio(inicio), fechaFin(fin) {}
+
+void Reserva::mostrarDetallesReserva() {
+    cout << "Reserva ID: " << idReserva << endl;
+    cliente.mostrarInfoCliente();
+    cout << "Fecha de Inicio: " << fechaInicio << ", Fecha de Fin: " << fechaFin << endl;
