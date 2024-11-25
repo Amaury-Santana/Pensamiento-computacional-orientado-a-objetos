80737dae519114a0e561d62472b1b8d23d5f6092
Amaury-Santana <amaurysantana070@gmail.com>
2024-11-24 22:20:59 -0600

    cliente.cpp

--- cliente.cpp
+++ cliente.cpp
@@ -2,13 +2,19 @@
 #include <iostream>
 using namespace std;
 
-//Definir que el constructor pertenece a la clase Cliente
 Cliente::Cliente(string n, string t, int e, string fp) : nombre(n), telefono(t), edad(e), formaDePago(fp) {}
 
-void Cliente::mostrarInfoCliente() {
-    cout << "Nombre: " << nombre << ", Teléfono: " << telefono << ", Edad: " << edad
+void Cliente::mostrarInfoCliente() const {
+    cout << "Nombre: " << nombre << ", Telefono: " << telefono << ", Edad: " << edad 
          << ", Forma de Pago: " << formaDePago << endl;
 }
 
-string Cliente::getNombre() { return nombre; }
-int Cliente::getEdad() { return edad; }
+string Cliente::getNombre() const { return nombre; }
+int Cliente::getEdad() const { return edad; }
+string Cliente::getFormaDePago() const { return formaDePago; }
+
+void Cliente::setNombre(string n) { nombre = n; }
+void Cliente::setTelefono(string t) { telefono = t; }
+void Cliente::setEdad(int e) { edad = e; }
+void Cliente::setFormaDePago(string fp) { formaDePago = fp; }
+
