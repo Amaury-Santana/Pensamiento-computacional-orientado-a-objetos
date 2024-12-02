274f24cf4d8794f5677a9eaad1f6919b7c30b756
Amaury-Santana <amaurysantana070@gmail.com>
2024-12-01 18:46:44 -0600

    cliente.h

--- cliente.h
+++ cliente.h
@@ -4,23 +4,24 @@
 #include <string>
 using namespace std;
 
+// Clase que representa a un cliente
 class Cliente {
 private:
-    string nombre;
-    string telefono;
-    int edad;
-    string formaDePago;
+    string nombre;        
+    string telefono;      
+    int edad;             
+    string formaDePago;   
 
 public:
-    Cliente(string, string, int, string);
-    void mostrarInfoCliente() const;
-    string getNombre() const;
-    int getEdad() const;
-    string getFormaDePago() const;
-    void setNombre(string);
-    void setTelefono(string);
-    void setEdad(int);
-    void setFormaDePago(string);
+    Cliente(string, string, int, string); // Constructor
+    void mostrarInfoCliente() const;     
+    string getNombre() const;            
+    int getEdad() const;                 
+    string getFormaDePago() const;       
+    void setNombre(string);             
+    void setTelefono(string);          
+    void setEdad(int);                   
+    void setFormaDePago(string);         
 };
 
 #endif
