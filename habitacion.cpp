6846dce9d022c2487669b51fc55e763f2e2bd8d3
Amaury-Santana <amaurysantana070@gmail.com>
2024-12-01 18:45:31 -0600

    habitacion.cpp

--- habitacion.cpp
+++ habitacion.cpp
@@ -2,16 +2,27 @@
 #include <iostream>
 using namespace std;
 
+// Constructor
 Habitacion::Habitacion(int num, string t, bool disp, float p)
     : numeroHabitacion(num), tipo(t), disponible(disp), precio(p) {}
 
-void Habitacion::cambiarDisponibilidad(bool estado) { disponible = estado; }
-
-void Habitacion::mostrarInfoHabitacion() const {
-    cout << "Habitacion No: " << numeroHabitacion << ", Tipo: " << tipo
-         << ", Disponible: " << (disponible ? "Si" : "No")
-         << ", Precio: " << precio << endl;
+// Cambia la disponibilidad
+void Habitacion::cambiarDisponibilidad(bool estado) {
+    disponible = estado;
 }
 
-float Habitacion::getPrecio() const { return precio; }
-void Habitacion::setPrecio(float p) { precio = p; }
+// Muestra información de la habitación
+void Habitacion::mostrarInfoHabitacion() const {
+    cout << "Habitacion No: " << numeroHabitacion << endl;
+    cout << "Tipo: " << tipo << endl;
+    cout << "Disponible: " << (disponible ? "Si" : "No") << endl;
+    cout << "Precio: $" << precio << endl;
+}
+
+float Habitacion::getPrecio() const {
+    return precio;
+}
+
+void Habitacion::setPrecio(float p) {
+    precio = p;
+}
