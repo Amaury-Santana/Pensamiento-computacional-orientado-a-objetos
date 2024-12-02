e9cbf569781bb9f5e6e75dacc7918fe33e3aae1e
Amaury-Santana <amaurysantana070@gmail.com>
2024-12-01 18:44:42 -0600

    reserva.h

--- reserva.h
+++ reserva.h
@@ -4,22 +4,24 @@
 #include "cliente.h"
 #include "habitacion.h"
 #include <string>
+#include <vector>
 using namespace std;
 
+// Clase base para manejar reservas
 class Reserva {
 protected:
-    int idReserva;
-    Cliente cliente;
-    Habitacion habitacion;
-    string fechaInicio;
-    string fechaFin;
+    int idReserva;                     
+    Cliente* cliente;                  
+    vector<Habitacion*> habitaciones;  
+    string fechaInicio;                
+    string fechaFin;                   
 
 public:
-    Reserva(int, Cliente, Habitacion, string, string);
-    virtual void mostrarDetallesReserva() const;
-    float calcularCostoTotal() const;
-    void setFechaInicio(string);
-    void setFechaFin(string);
+    Reserva(int, Cliente*, vector<Habitacion*>, string, string); // Constructor
+    virtual ~Reserva(); 
+    virtual void mostrarDetallesReserva() const; 
+    virtual string getTipoReserva() const; 
+    float calcularCostoTotal() const;     
 };
 
 #endif
