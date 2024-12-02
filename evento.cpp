dacd7ebd633d84f5d289aab72b35e92a7858eb4c
Amaury-Santana <amaurysantana070@gmail.com>
2024-12-01 18:46:18 -0600

    evento.cpp

--- evento.cpp
+++ evento.cpp
@@ -2,14 +2,20 @@
 #include <iostream>
 using namespace std;
 
-Evento::Evento(int id, Cliente c, Habitacion h, string inicio, string fin, string nombre, string desc, int asis)
-    : Reserva(id, c, h, inicio, fin), nombreEvento(nombre), descripcion(desc), asistentes(asis) {}
+// Constructor
+Evento::Evento(int id, Cliente* c, vector<Habitacion*> habs, string inicio, string fin,
+               string nombre, string desc, int asis)
+    : Reserva(id, c, habs, inicio, fin), nombreEvento(nombre), descripcion(desc), asistentes(asis) {}
 
+// Muestra detalles del evento
 void Evento::mostrarDetallesReserva() const {
-    Reserva::mostrarDetallesReserva();
-    cout << "Nombre del Evento: " << nombreEvento << ", Descripcion: " << descripcion
-         << ", Asistentes: " << asistentes << endl;
+    Reserva::mostrarDetallesReserva(); // Muestra detalles generales de la reserva
+    cout << "Nombre del Evento: " << nombreEvento << endl;
+    cout << "Descripcion: " << descripcion << endl;
+    cout << "Asistentes: " << asistentes << endl;
 }
 
-void Evento::setNombreEvento(string nombre) { nombreEvento = nombre; }
-void Evento::setDescripcion(string desc) { descripcion = desc; }
+// Lo identifica como evento
+string Evento::getTipoReserva() const {
+    return "Evento";
+}
