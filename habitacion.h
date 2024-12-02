96af52eb1cdca92613f22fc08ab2c2032c5e854f
Amaury-Santana <amaurysantana070@gmail.com>
2024-12-01 18:45:05 -0600

    habitacion.h

--- habitacion.h
+++ habitacion.h
@@ -4,19 +4,20 @@
 #include <string>
 using namespace std;
 
+// Clase que representa una habitación
 class Habitacion {
 private:
-    int numeroHabitacion;
-    string tipo;
-    bool disponible;
-    float precio;
+    int numeroHabitacion; 
+    string tipo;          
+    bool disponible;      
+    float precio;         
 
 public:
-    Habitacion(int, string, bool, float);
-    void cambiarDisponibilidad(bool);
-    void mostrarInfoHabitacion() const;
-    float getPrecio() const;
-    void setPrecio(float);
+    Habitacion(int, string, bool, float); // Constructor
+    void cambiarDisponibilidad(bool);    
+    void mostrarInfoHabitacion() const;  
+    float getPrecio() const;             
+    void setPrecio(float);               
 };
 
 #endif
