5cdf3e16e8031c540def535cfdff139cd4aa2cc4
Amaury-Santana <amaurysantana070@gmail.com>
2024-12-01 18:47:15 -0600

    cliente.cpp

--- cliente.cpp
+++ cliente.cpp
@@ -2,19 +2,36 @@
 #include <iostream>
 using namespace std;
 
-Cliente::Cliente(string n, string t, int e, string fp) : nombre(n), telefono(t), edad(e), formaDePago(fp) {}
+// Constructor
+Cliente::Cliente(string n, string t, int e, string fp)
+    : nombre(n), telefono(t), edad(e), formaDePago(fp) {}
 
+// Muestra detalles del cliente
 void Cliente::mostrarInfoCliente() const {
-    cout << "Nombre: " << nombre << ", Telefono: " << telefono << ", Edad: " << edad 
-         << ", Forma de Pago: " << formaDePago << endl;
+    cout << "Nombre: " << nombre << endl;
+    cout << "Telefono: " << telefono << endl;
+    cout << "Edad: " << edad << endl;
+    cout << "Forma de Pago: " << formaDePago << endl;
 }
 
-string Cliente::getNombre() const { return nombre; }
-int Cliente::getEdad() const { return edad; }
-string Cliente::getFormaDePago() const { return formaDePago; }
-
-void Cliente::setNombre(string n) { nombre = n; }
-void Cliente::setTelefono(string t) { telefono = t; }
-void Cliente::setEdad(int e) { edad = e; }
-void Cliente::setFormaDePago(string fp) { formaDePago = fp; }
-
+string Cliente::getNombre() const {
+    return nombre;
+}
+int Cliente::getEdad() const {
+    return edad;
+}
+string Cliente::getFormaDePago() const {
+    return formaDePago;
+}
+void Cliente::setNombre(string n) {
+    nombre = n;
+}
+void Cliente::setTelefono(string t) {
+    telefono = t;
+}
+void Cliente::setEdad(int e) {
+    edad = e;
+}
+void Cliente::setFormaDePago(string fp) {
+    formaDePago = fp;
+}
